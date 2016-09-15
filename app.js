e762b97662e094f397efee3506be1631c0a21115
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2016-09-15 19:56:22 +0300

    Load fixtures from server.

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
-var express = require('express');
-var path = require('path');
-
-var portNumber = 3000;
+var express = require("express");
+var path = require("path");
 
 var app = express();
+var portNumber = 3000;
 
-app.set('port', portNumber);
-app.use('/', express.static(path.join(__dirname, 'public')));
+app.set("port", portNumber);
+app.use("/", express.static(path.join(__dirname, "public")));
+
+var fixtures = [
+	{ "id": 1, "date": "11.09.2016", "amount": 27, "description": "Catering", "category": "Food" },
+	{ "id": 2, "date": "11.09.2016", "amount": 292, "description": "Flight tickets", "category": "Travel" },
+	{ "id": 3, "date": "12.09.2016", "amount": 555, "description": "Rent", "category": "Monthly expenses" }
+];
+
+app.get("/api/v1/expenses", function (req, res) {
+	res.send(fixtures);
+});
 
 app.listen(portNumber, function() {
-	console.log('Server started');
+	console.log("Server started");
 });
