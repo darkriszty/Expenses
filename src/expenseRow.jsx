41405d146fd22fe8a192ab131e55c7e1edb8f6c4
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2016-09-16 18:11:22 +0300

    Add the possibility to edit row data.

--- src/expenseRow.jsx
+++ src/expenseRow.jsx
@@ -1,13 +1,88 @@
 var React = require('react');
+var moment = require('moment');
 
 var ExpenseRow = React.createClass({
+	getInitialState: function() {
+		return this.getDefaultState();
+	},
+	getDefaultState: function() {
+		return {
+			isEditing: false,
+			rowData: {
+				date: this.props.date,
+				amount: this.props.amount,
+				description: this.props.description,
+				category: this.props.category
+			}
+		};
+	},
+	handleDateChange: function(e) {
+		var val = e.target.value;
+		this.setState(s => {
+			s.rowData.date = moment(val, "YYYY-MM-DD");
+			return s
+		});
+	},
+	handleAmountChange: function(e) {
+		var val = e.target.value;
+		this.setState(s => {
+			s.rowData.amount = val;
+			return s
+		});
+	},
+	handleDescriptionChange: function(e) {
+		var val = e.target.value;
+		this.setState(s => {
+			s.rowData.description = val;
+			return s
+		});
+	},
+	handleCategoryChange: function(e) {
+		var val = e.target.value;
+		this.setState(s => {
+			s.rowData.category = val;
+			return s
+		});
+	},
+	handleEditModeChange: function(e) {
+		e.preventDefault();
+		var description = this.state.rowData.description.trim();
+		var category = this.state.rowData.category.trim();
+		if (!description || !category) {
+			alert("Description and category can't be null");
+			return;
+		}
+
+		//TODO: submit update to server
+		this.setState(s => {
+			s.isEditing = !s.isEditing;
+			return s
+		});
+	},
 	render: function() {
+		var formattedDate = "";
+		if (this.state.isEditing) {
+			formattedDate = moment(this.state.rowData.date, "DD-MM-YYYY").format("YYYY-MM-DD").toString();
+			return (
+				<tr>
+					<td><input type="date" value={formattedDate} onChange={this.handleDateChange} /></td>
+					<td><input type="number" min="0" step="any" placeholder="amount" value={this.state.rowData.amount} onChange={this.handleAmountChange} /></td>
+					<td><input type="text" placeholder="description" value={this.state.rowData.description} onChange={this.handleDescriptionChange} /></td>
+					<td>
+						<input type="text" placeholder="category" value={this.state.rowData.category} onChange={this.handleCategoryChange} />
+						<input type="submit" value="Update" onClick={this.handleEditModeChange} />
+					</td>
+				</tr>
+			);
+		}
+
+		formattedDate = moment(this.state.rowData.date, "DD.MM.YYYY", true).format("DD.MM.YYYY");
 		return (
-			<tr>
-				<td>{this.props.date}</td>
-				<td>{this.props.amount}</td>
-				<td>{this.props.description}</td>
-				<td>{this.props.category}</td>
+			<tr onClick={this.handleEditModeChange}>
+				<td>{formattedDate}</td>
+				<td>{this.state.rowData.amount}</td>
+				<td>{this.state.rowData.description}</td>
+				<td>{this.state.rowData.category}</td>
 			</tr>
 		);
 	}
