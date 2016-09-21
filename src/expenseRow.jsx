95f642578ee3b696fc8b6a572640cc7824b4a2f2
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2016-09-21 18:27:31 +0300

    Add the possibility to cancel changes on the row that's being edited.

--- src/expenseRow.jsx
+++ src/expenseRow.jsx
@@ -16,52 +16,77 @@ var ExpenseRow = React.createClass({
 				amount: this.props.amount,
 				description: this.props.description,
 				category: this.props.category
+			},
+			editedExpenseData: {
+				date: moment(this.props.date, DISPLAY_FORMAT),
+				amount: this.props.amount,
+				description: this.props.description,
+				category: this.props.category
 			}
 		};
 	},
 	handleDateChange: function(e) {
 		var val = e.target.value;
 		this.setState(s => {
-			s.expenseData.date = moment(val, EDIT_FORMAT);
+			s.editedExpenseData.date = moment(val, EDIT_FORMAT);
 			return s;
 		});
 	},
 	handleAmountChange: function(e) {
 		var val = e.target.value;
 		this.setState(s => {
-			s.expenseData.amount = val;
+			s.editedExpenseData.amount = val;
 			return s;
 		});
 	},
 	handleDescriptionChange: function(e) {
 		var val = e.target.value;
 		this.setState(s => {
-			s.expenseData.description = val;
+			s.editedExpenseData.description = val;
 			return s;
 		});
 	},
 	handleCategoryChange: function(e) {
 		var val = e.target.value;
 		this.setState(s => {
-			s.expenseData.category = val;
+			s.editedExpenseData.category = val;
 			return s;
 		});
 	},
 	handleEditModeChange: function(e) {
 		e.preventDefault();
-		var description = this.state.expenseData.description.trim();
-		var category = this.state.expenseData.category.trim();
+		var description = this.state.editedExpenseData.description.trim();
+		var category = this.state.editedExpenseData.category.trim();
 		if (!description || !category) {
 			alert("Description and category can't be null");
 			return;
 		}
 
-		this.props.onExpenseUpdated(this.state.expenseData);
-
 		this.setState(s => {
 			s.isEditing = !s.isEditing;
+			s.expenseData.date = s.editedExpenseData.date;
+			s.expenseData.amount = s.editedExpenseData.amount;
+			s.expenseData.description = s.editedExpenseData.description;
+			s.expenseData.category = s.editedExpenseData.category;
 			return s;
 		});
+
+		this.props.onExpenseUpdated(this.state.expenseData);
+	},
+	handleCancel: function(e) {
+		e.preventDefault();
+		this.setState(s => {
+			s.isEditing = false;
+			s.editedExpenseData.date = s.expenseData.date;
+			s.editedExpenseData.amount = s.expenseData.amount;
+			s.editedExpenseData.description = s.expenseData.description;
+			s.editedExpenseData.category = s.expenseData.category;
+			return s;
+		});
+	},
+	handleInputKeyDown: function(e) {
+		if (e.keyCode === 13)
+			this.handleEditModeChange(e);
 	},
 	render: function() {
 		var formattedDate = "";
@@ -69,12 +94,13 @@ var ExpenseRow = React.createClass({
 			formattedDate = this.state.expenseData.date.format(EDIT_FORMAT).toString();
 			return (
 				<tr>
-					<td><input type="date" value={formattedDate} onChange={this.handleDateChange} /></td>
-					<td><input type="number" min="0" step="any" placeholder="amount" value={this.state.expenseData.amount} onChange={this.handleAmountChange} /></td>
-					<td><input type="text" placeholder="description" value={this.state.expenseData.description} onChange={this.handleDescriptionChange} /></td>
+					<td><input type="date" value={formattedDate} onChange={this.handleDateChange} onKeyDown={this.handleInputKeyDown} /></td>
+					<td><input type="number" min="0" step="any" placeholder="amount" value={this.state.editedExpenseData.amount} onChange={this.handleAmountChange} onKeyDown={this.handleInputKeyDown} /></td>
+					<td><input type="text" placeholder="description" value={this.state.editedExpenseData.description} onChange={this.handleDescriptionChange} onKeyDown={this.handleInputKeyDown} /></td>
 					<td>
-						<input type="text" placeholder="category" value={this.state.expenseData.category} onChange={this.handleCategoryChange} />
+						<input type="text" placeholder="category" value={this.state.editedExpenseData.category} onChange={this.handleCategoryChange} onKeyDown={this.handleInputKeyDown} />
 						<input type="submit" value="Update" onClick={this.handleEditModeChange} />
+						<input type="submit" value="Cancel" onClick={this.handleCancel} />
 					</td>
 				</tr>
 			);
